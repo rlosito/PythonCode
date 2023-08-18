8d71e4e1b43c26a57258fe8ccf21796fbffb1954
rlosito <rafael.losito@gmail.com>
2023-08-19 01:34:30 +0200

    funciones 1

--- Intro.py
+++ Intro.py
@@ -24,7 +24,44 @@ else:
     print("No es un numero")
 
 
+# funciones de python
 
+def Saludar():
+    print("primera funcion")
 
+# para usar la funcion
+Saludar()
+    
+
+# Funcion con argumentos
+def saludardos(nombre):
+    print("hola " + nombre + " que tal?")
+
+saludardos("Pepe")
+saludardos("M.Rajoy")
+
+# Funcion con retorno
+def suma(a,b):
+    resultado = a + b 
+    return resultado
+
+valor = suma(10,5)
+print(valor)
+
+# funcion con valor por defecto 
+def Saludarpordefecto(nombre="Loso"):
+    print("Hola " + nombre + "que tal ?")
+
+Saludarpordefecto() # utilizara el valor por defecto
+Saludarpordefecto("Pepe") # utilizara el valor pepe
+
+# funcion que retorna varios valores
+def sumayresta(a,b):
+    suma = a + b 
+    resta = a - b 
+    return suma, resta
+
+resultado1, resultado2 = sumayresta(10,5)
+print("Los resultado son:\nSunma:" + str(resultado1) + "\nResta:" + str(resultado2))
 
 
