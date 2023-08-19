a7b1e4c50c791524e98ba866bce992cad9a34d8e
rlosito <rafael.losito@gmail.com>
2023-08-20 01:12:26 +0200

    blucles primera parte

--- Intro.py
+++ Intro.py
@@ -62,7 +62,6 @@ def sumayresta(a,b):
 resultado1, resultado2 = sumayresta(10,5)
 print("Los resultado son:\nSunma:" + str(resultado1) + "\nResta:" + str(resultado2))
 
-"""
 # Ejercicio 1 de funciones 
 def Esnumero (num):
     if (type(num)==int):
@@ -87,4 +86,70 @@ if (Esnumero(Num1) and Esnumero(Num2)):
 else:
     print("una de las variables no es un numero")
 
- 
+
+
+# -Introduccion a la lista.
+numeros = [1,2,3,4,5]  # como las tablas la primera posicion del indice es la cero. 
+# para acceder a un elemento de la lista 
+numeros[0] # hace referencia al primer elemento de la lista llamada numeros.
+len(numeros) # para obtener el numero de elementos de la lista.
+
+# Bucles for
+for x in range(5):  # con solo un argumento indica que empieza en 0 y llega hasta 4 (son 5 iteraciones) 0,1,2,3,4
+    print(x)
+
+for x in range(5,10):  # con dos argumentos indica limite superior e infierior: empiez en el 5 y termina en el 9 --> 5,6,7,8,9
+    print(x)
+
+for x in range(5,20,2):  # con tres argumentos indica limite superior e infierior y el incremento: empieza en el 5 y termina en el 19 --> 5,7,9,11,13,15,17,19
+    print(x)
+
+for x in range(5,21,2):  # empieza 5,7,9,11,13,15,17,19 como el limite es el valor superior -1 (21 en este caso) no es alcanzable la ultima iteracion no se hace. 
+    print(x)
+
+# mini ejemplo, sacar las vocales de una palabra:
+palabra = "hola"
+for letra in palabra:
+    if (letra == "a" or letra == "e" or letra=="i"or letra=="o"or letra=="u"):
+        print(letra)
+    else:
+        print("Es una consonante")
+""" 
+
+# mini ejemplo, trabajar con lista dentro del bucle. 
+"""numeros = [1,2,3,4,5]
+for numero in numeros:
+    print(numero)
+    numero += 10
+    print(numero)"""
+
+"""print(numeros)"""
+# conclusion no hemos modificado el valor de la lista 
+
+"""for indice in range(len(numeros)):
+    numeros[indice] += 10
+print(numeros)"""
+# conclusion aqui si hemos cambiado los valores de la lista 
+
+
+# BUCLE WHILE
+"""cont = 0
+while(cont < 10):
+    print(cont)
+    cont += 1"""
+
+LetraEncontrada = False
+letra = "a"
+frase = "En este momento estoy buscando la letra a"
+indice = 0
+
+while( not(LetraEncontrada)):
+    if(frase[indice] == letra):
+        LetraEncontrada = True
+    else:
+        indice += 1
+
+print (f"letra encontrada en posicion : {indice} ")
+
+
+
