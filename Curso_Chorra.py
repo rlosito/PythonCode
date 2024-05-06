469c3e7d14f151701e9accaf5c928582a3e815b2
rlosito <rafael.losito@gmail.com>
2024-05-06 22:00:21 +0200

    curso

---
+++ Curso_Chorra.py
@@ -0,0 +1,20 @@
+'''
+dsakfljasdflkjfasdf
+asdkfjñasdlkfj
+asñdlkfjasdlfj
+'''
+
+
+def minimo(l):
+    l[0] = 1000
+    return min(l)
+
+l = [1,2,3,4]
+print(l)
+
+print(minimo(l[:]))
+
+print(l)
+
+s_texto = "esto es una prueba"
+print (s_texto.upper())
